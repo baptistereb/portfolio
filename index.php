d414a681d9aa03c1e183e58f0ea8709c2315f780
Baptiste <rebillar@insa-toulouse.fr>
2022-07-02 18:13:52 +0200

    section footer/contact terminé

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$fr = ["fr", "Français", "English", "Accueil", "Projets", "Compétences", "Contact", "Etudiant à l'INSA Toulouse, développeur web", "Amoureux des imprimantes 3D et plus globalement des nouvelles technologies (communement appelé : nerd)"];
-$en = ["en", "English", "Français", "Home", "Projects", "Skills", "Contact", "Student at INSA Toulouse, web developer", "Lover of 3D printers and more generally of new technologies (commonly called : a nerd)"];
+$fr = ["fr", "Français", "English", "Accueil", "Projets", "Compétences", "Contact", "Etudiant à l'INSA Toulouse, développeur web", "Amoureux des imprimantes 3D, de programmation et plus globalement des nouvelles technologies (communement appelé : nerd)", "Me contacter", "Retourner à la page d'accueil", "Tous droits réservés"];
+$en = ["en", "English", "Français", "Home", "Projects", "Skills", "Contact", "Student at INSA Toulouse, web developer", "Lover of 3D printers, programming and more generally of new technologies (commonly called : a nerd)", "Contact me", "Back to homepage", "All rights reserved"];
 
 function index() {
 	header('Location: #');
@@ -118,27 +118,48 @@ if(isset($_POST['language'])) {
 
 	-->
 </section>
-<section class="contact" id="contact" align="center">
-	<!--linkedin
-	mail : baptiste.rebillard@proton-mail.com
-	-->
-	<a href="mailto:baptiste.rebillard@protonmail.com" class="a-button">
-		<img src="img/mail.svg" class="title-button">
-		<div class="content">
-			<div class="table">
-				<div class="cell">baptiste.rebillard@protonmail.com</div>
+<section class="contact" id="contact" style="text-align:center;">
+	<div class="title-contact"><?= $v['9'] ?></div>
+
+	<div id="center_perfect">
+		<a href="mailto:baptiste.rebillard@protonmail.com" class="a-button">
+			<img src="img/mail.svg" class="title-button">
+			<div class="content">
+				<div class="table">
+					<div class="cell">baptiste.rebillard@protonmail.com</div>
+				</div>
 			</div>
-		</div>
+		</a>
+
+		<a href="https://www.linkedin.com/in/baptistereb/" target="_linkedin" class="a-button">
+			<img src="img/linkedin.svg" class="title-button">
+			<div class="content">
+				<div class="table">
+					<div class="cell">linkedin.com/in/baptistereb</div>
+				</div>
+			</div>
+		</a>
+
+		<a href="https://github.com/baptistereb" target="_github" class="a-button">
+			<img src="img/git.svg" class="title-button">
+			<div class="content">
+				<div class="table">
+					<div class="cell">github.com/baptistereb</div>
+				</div>
+			</div>
+		</a>
+	</div>
+
+	<a href="#index" class="back-home">
+		<li class="up">
+			<div id="arrow-top"></div>
+			<div id="text-top"><?= $v[10] ?></div>
+		</li>
 	</a>
 
-	<a href="https://www.linkedin.com/in/baptistereb/" target="_linkedin" class="a-button">
-		<img src="img/linkedin.svg" class="title-button">
-		<div class="content">
-			<div class="table">
-				<div class="cell">linkedin.com/in/baptistereb</div>
-			</div>
-		</div>
-	</a>
+	<div id="copyright">
+		<span>Copyright © 2022 - Rébillard Baptiste - <?= $v['11'] ?></span>
+	</div>
 </section>
 
 </body>
